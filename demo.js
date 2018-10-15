9bc75413fdaa5d1946ba4dadebb61e06d4585d62
zhaoshiwei <283308372@qq.com>
2018-10-15 23:49:19 +0800

    first

--- demo.js
+++ demo.js
@@ -1,9 +1,20 @@
 function init() {
     var oDiv = document.getElementsByClassName('item');
+    var btn = document.getElementsByClassName('btn')[0];
     for (let i = 0; i < oDiv.length; i++) {
-        oDiv[i].onclick = function() {
-            console.log(13)
+        oDiv[i].onclick = function(e) {
+            if (oDiv[i].style.border == "") {
+                oDiv[i].style.border = "2px solid #fff"
+            } else {
+                oDiv[i].style.border = "2px dashed #fff"
+            }
+
         }
+    }
+
+
+    btn.onclick = function() {
+        oDiv.style.border = "2px dashed #fff"
     }
 }
 init()
